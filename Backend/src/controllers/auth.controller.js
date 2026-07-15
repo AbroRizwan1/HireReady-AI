96b8d0e38725e6899a419147b3ea6e1f2cc0a04f
AbroRizwan1 <Rizwanabro14@gmail.com>
2026-07-15 16:48:56 +0500

    Downgrade pdf-parse for Vercel compatibility

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -99,7 +99,12 @@ const userLogin = async (req, res) => {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
   res.status(200).json({
     message: "user logged in successfully",
@@ -124,8 +129,12 @@ const userlogout = async (req, res) => {
     await tokenBlackListModel.create({ token });
   }
 
-  res.clearCookie("token");
-
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+  
   res.status(200).json({
     message: "user logged out successfully",
   });
