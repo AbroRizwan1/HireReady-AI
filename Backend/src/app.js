3031512c215205d4758b5c429073e86d1454a7ad
AbroRizwan1 <Rizwanabro14@gmail.com>
2026-07-15 14:09:22 +0500

    Configure production deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://hire-ready-ai-ye3c.vercel.app/",
+    origin: "https://hire-ready-ai-a66z.vercel.app/",
     credentials: true,
   }),
 );
