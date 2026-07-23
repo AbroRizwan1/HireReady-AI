48e77f2561f96d3fbf7f446a4af6cd6fa4549778
AbroRizwan1 <Rizwanabro14@gmail.com>
2026-07-23 18:16:48 +0500

    fix: pdf parser and auth id casting error

--- Frontend/src/Features/Interview/pages/Interview.jsx
+++ Frontend/src/Features/Interview/pages/Interview.jsx
@@ -274,19 +274,6 @@ export default function InterviewPage(props) {
     )
   }
 
-  if (!report) {
-    return (
-      <main>
-        <div style={{ textAlign: "center", padding: "2rem" }}>
-          <p>{error || "Report not found or failed to load."}</p>
-          <button onClick={() => getReportById(interviewId)}>
-            Retry
-          </button>
-        </div>
-      </main>
-    );
-  }
-
 
   return (
     <div style={{ background: "#37353E" }} className="min-h-screen flex item-center justify-center px-3 sm:px-4 py-6 sm:py-12 overflow-hidden relative" >
